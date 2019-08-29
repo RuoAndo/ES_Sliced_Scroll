94c7294749abd397456b9ca189d80af1477d8eed
Ruo Ando <ando.ruo@gmail.com>
2019-08-29 09:47:44 +0900

    gen

---
+++ generator/gen.sh
@@ -0,0 +1,21 @@
+if [ "$1" = "" ]
+then
+    echo "usage: ./test nLines nFiles"
+    exit 1
+fi
+
+nLines=$1
+nFiles=$2
+
+rm -rf random_data-all
+touch random_data-all
+
+for i in `seq 1 $nFiles`
+do
+    echo "$i 回目のループです。"
+    time ./9 $nLines
+    num=`expr $nLines \* $nFiles`
+    cat random_data.txt >> random_data-all
+done
+
+wc -l random_data-all
