2063505d6b127efc1e43c6d4bce106ac88b21dac
Ruo Ando <ando.ruo@gmail.com>
2019-10-13 13:37:36 +0900

    free()

--- discern/gpu/discernGPU.cpp
+++ discern/gpu/discernGPU.cpp
@@ -45,8 +45,8 @@ using namespace std;
 using namespace tbb;
 
 // 2 / 1024
-#define WORKER_THREAD_NUM 5
-#define MAX_QUEUE_NUM 17
+#define WORKER_THREAD_NUM 33
+#define MAX_QUEUE_NUM 65
 #define END_MARK_FNAME   "///"
 #define END_MARK_FLENGTH 3
 
