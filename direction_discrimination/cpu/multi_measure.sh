f305b09754747fd9c840bd974b16199f0057f586
Ruo Ando <ando.ruo@gmail.com>
2019-11-09 16:01:56 +0900

    shell

--- direction_discrimination/cpu/multi_measure.sh
+++ direction_discrimination/cpu/multi_measure.sh
@@ -1,9 +1,11 @@
-date=$(date -d '2 day ago' "+%Y%m%d")
+date=$(date -d '1 day ago' "+%Y%m%d")
 echo $date
 REGION_NAME="ap-southeast-1"
 
+./build.sh multi_measure
+
 echo "copying..."
-time cp -r /data1/${date} .
+time cp -r /root/${date} .
 time ./multi_measure $date list-${REGION_NAME}
 
 mkdir ${date}_ingress_${REGION_NAME}
