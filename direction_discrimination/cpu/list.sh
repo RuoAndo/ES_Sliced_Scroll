0403a42a75250a64e5f8efbbba702d0430b11348
Ruo Ando <ando.ruo@gmail.com>
2019-11-10 16:32:10 +0900

    shell

--- direction_discrimination/cpu/list.sh
+++ direction_discrimination/cpu/list.sh
@@ -1,6 +1,9 @@
 date=$(date -d '2 day ago' "+%Y%m%d")
 echo $date
-REGION_NAME="eu-west-1"
+REGION_NAME="ap-southeast-1"
+
+mkdir egress
+mkdir ingress
 
 ls ./${date}/*ingress > list
 
@@ -9,6 +12,7 @@ while read line; do
     fn_dst=`echo $line | cut -d "/" -f 3`
     cat header > tmp
     cat ${fn_src} >> tmp
+    echo "./ingress/${REGION_NAME}_${fn_dst}_${date}"
     mv tmp ./ingress/${REGION_NAME}_${fn_dst}_${date}
 done < list
 
@@ -19,6 +23,7 @@ while read line; do
     fn_dst=`echo $line | cut -d "/" -f 3`
     cat header > tmp
     cat ${fn_src} >> tmp
+    echo "./egress/${REGION_NAME}_${fn_dst}_${date}"
     mv tmp ./egress/${REGION_NAME}_${fn_dst}_${date}
 done < list
 
--- direction_discrimination/cpu/multi_measure.sh
+++ direction_discrimination/cpu/multi_measure.sh
@@ -14,7 +14,8 @@ while read line; do
     fn_src=`echo $line`
     fn_dst=`echo $line | cut -d "/" -f 3`
     cat header > tmp
-    cat ${fn_src} >> tmp 
+    cat ${fn_src} >> tmp
+    echo "./ingress/${REGION_NAME}_${fn_dst}_${date}"
     mv tmp ./ingress/${REGION_NAME}_${fn_dst}_${date}
 done < list
 
@@ -23,6 +24,9 @@ ls ./${date}/*egress > list
 while read line; do
     fn_src=`echo $line`
     fn_dst=`echo $line | cut -d "/" -f 3`
-    cp ${fn_src} ./egress/${REGION_NAME}_${fn_dst}_${date}
+    cat header > tmp
+    cat ${fn_src} >> tmp
+    echo "./egress/${REGION_NAME}_${fn_dst}_${date}"
+    mv tmp ./egress/${REGION_NAME}_${fn_dst}_${date}
 done < list
 
