1f5dc05f57b65e17beb831d3981ecd4c172e1860
Ruo Ando <ando.ruo@gmail.com>
2019-11-12 13:09:40 +0900

    shell

--- histogramming_by_reduction/cpu/multi_measure.sh
+++ histogramming_by_reduction/cpu/multi_measure.sh
@@ -14,8 +14,8 @@ cp -r /mnt/data/Usenix_LISA19/direction_discrimination/cpu/sinet_ingress_${date}
 ./multi_measure ./sinet_ingress_${date}
 
 echo "timestamp, count" > tmp
-cat tmp-count >> tmp
-cp tmp egress_reduced/reduced_{$date}_egress_sinet
+cat tmp-counts >> tmp
+cp tmp egress_reduced/${date}_egress_sinet
 
 ./build.sh multi_measure
 echo "copying..."
@@ -24,7 +24,7 @@ cp -r /mnt/data/Usenix_LISA19/direction_discrimination/cpu/sinet_egress_${date} 
 ./multi_measure ./sinet_egress_${date} .
 
 echo "timestamp, count" > tmp
-cat tmp-count >> tmp
-cp tmp ingress_reduced/{$date}_ingress_sinet 
+cat tmp-counts >> tmp
+cp tmp ingress_reduced/${date}_ingress_sinet 
 
 #rm -rf ${date}
