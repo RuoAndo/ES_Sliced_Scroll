919c59a2c42ba6944f4c8be1cde3a9c799e26eae
Ruo Ando <ando.ruo@gmail.com>
2019-11-25 12:43:48 +0900

    shell

---
+++ direction_discrimination/cpu/multi_measure_2.sh
@@ -0,0 +1,46 @@
+date=$(date -d '1 day ago' "+%Y%m%d")
+echo $date
+REGION_NAME=$1
+
+if [ "$1" = "" ]
+then
+    echo "usage: ./multi_measure_2.sh [REGION_NAME]"
+    exit 1
+fi
+
+mkdir ingress
+mkdir egress
+
+mkdir ingress_${REGION_NAME}_${date}
+mkdir egress_${REGION_NAME}_${date}
+
+./build.sh multi_measure
+
+echo "copying..."
+time cp -r /mnt/data/${date} .
+time ./multi_measure $date list-${REGION_NAME}
+
+ls ./${date}/*ingress > list
+
+while read line; do
+    fn_src=`echo $line`
+    fn_dst=`echo $line | cut -d "/" -f 3`
+    cat header > tmp
+    cat ${fn_src} >> tmp
+    echo "./ingress/${REGION_NAME}_${fn_dst}_${date}"
+    cp tmp ./ingress_${REGION_NAME}_${date}/${REGION_NAME}_${fn_dst}_${date}
+    mv tmp ./ingress/${REGION_NAME}_${fn_dst}_${date}
+done < list
+
+ls ./${date}/*egress > list
+
+while read line; do
+    fn_src=`echo $line`
+    fn_dst=`echo $line | cut -d "/" -f 3`
+    cat header > tmp
+    cat ${fn_src} >> tmp
+    echo "./egress/${REGION_NAME}_${fn_dst}_${date}"
+    cp tmp ./egress_${REGION_NAME}_${date}/${REGION_NAME}_${fn_dst}_${date}
+    mv tmp ./egress/${REGION_NAME}_${fn_dst}_${date}
+done < list
+
