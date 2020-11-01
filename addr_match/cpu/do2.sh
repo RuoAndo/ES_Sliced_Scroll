713b4dd4a772e7e20ad48a831c3d30908928a3ac
Ruo Ando <ando.ruo@gmail.com>
2020-11-01 17:30:40 +0900

    auto

--- addr_match/cpu/do2.sh
+++ addr_match/cpu/do2.sh
@@ -1,3 +1,3 @@
-./do.sh ingress_${1} | cut -d "," -f 22 | grep -v ip | tr -d "\"" | uniq > list2
+./do.sh ingress_${1} | cut -d "," -f 22 | grep -v ip | grep -v directory | tr -d "\"" | uniq > list2
 ./drem.pl list2 > list3
-./search.sh list3 list-${1} | uniq 
+./search.sh list3 list-${1} | uniq  > hit-${1}
