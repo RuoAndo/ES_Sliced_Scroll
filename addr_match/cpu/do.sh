61cec4065c90361f2486c84b294adbb576c21ff1
Ruo Ando <ando.ruo@gmail.com>
2020-11-01 17:34:52 +0900

    auto

--- addr_match/cpu/do.sh
+++ addr_match/cpu/do.sh
@@ -1,4 +1,4 @@
-ls -alh $1 | grep -v 604 | grep [a-z] | sed 's/[\t ]\+/\t/g' | cut -f 9 | uniq > list 
+ls -alh $1 | grep -v 604 |  sed 's/[\t ]\+/\t/g' | cut -f 9 | uniq > list 
 
 while read line; do
     cat ${1}/${line}
