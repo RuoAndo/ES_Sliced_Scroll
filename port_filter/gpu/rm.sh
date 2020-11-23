224f4924716dc0d18332d21c636a52c707c0951d
Ruo Ando <ando.ruo@gmail.com>
2020-11-23 16:48:42 +0900

    rm

--- port_filter/gpu/rm.sh
+++ port_filter/gpu/rm.sh
@@ -3,6 +3,15 @@ echo $RMDATE
 rm -rf *_${RMDATE}
 rm -rf $RMDATE
 
+RMDATE=`date --date '60 day ago' +%Y%m%d`
+
+rm -rf egress_sinet/*_${RMDATE}
+rm -rf ingress_sinet/*_${RMDATE}
+
+rm -rf egress_sinet_${RMDATE}
+rm -rf ingress_sinet_${RMDATE}
+
+
 #if [[ ! -e $RMDATE ]]; then
 #  echo "OK:"$RMDATE
 #fi
