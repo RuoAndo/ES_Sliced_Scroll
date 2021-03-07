4f11d73d5c801bca7ba4001314bf17a966673819
Ruo Ando <ando.ruo@gmail.com>
2021-03-08 03:36:52 +0900

    app

--- application/cpu/multi_measure_2.sh
+++ application/cpu/multi_measure_2.sh
@@ -22,6 +22,7 @@ mkdir egress_${REGION_NAME}_${date}
 ./build.sh multi_measure
 
 echo "copying..."
+time du -h /mnt/data/${date} 
 time cp -r /mnt/data/${date} .
 time ./multi_measure $date list-${REGION_NAME}
 
