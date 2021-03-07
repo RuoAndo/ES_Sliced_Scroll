8e2fd8b50d9bd79c1552d107aafea6c1d81164fd
Ruo Ando <ando.ruo@gmail.com>
2021-03-08 03:27:39 +0900

    category

---
+++ category/cpu/abuseip-do-300.sh
@@ -0,0 +1,10 @@
+cp /root/blacklist.txt .
+
+g++ -o rand rand.cpp -std=c++11
+./rand 300 > list-rand
+./listgen.sh list-rand > list-abuseipdb
+
+#shuf -n 1000 blacklist.txt > tmp
+#./append.sh tmp | tee list-abuseipdb
+
+./multi_measure_2.sh abuseipdb
